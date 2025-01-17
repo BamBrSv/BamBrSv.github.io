132ff31650d761b91010ede89b87bfb76af54185
walentyao <prikolnyyyvalk@yandex.ru>
2025-01-17 03:52:06 +0300

    feat: Ваниль

--- src/pages/Ind/Ind.tsx
+++ src/pages/Ind/Ind.tsx
@@ -10,9 +10,16 @@ export const Ind = () => {
   return (
     <section aria-label="Ind" className={cls.ind}>
       <h3 className={cls.indTitle}>{data.title}</h3>
-      <video className={cls.indVideo} controls poster={data.preview}>
-        <source src={data.video} type="video/mp4" />
-      </video>
+      <div>
+        <iframe
+          src="https://vkvideo.ru/video_ext.php?oid=-29903964&id=456240845&hd=2&autoplay=1"
+          width="853"
+          height="480"
+          allow="autoplay; encrypted-media; fullscreen; picture-in-picture; screen-wake-lock;"
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
+      </div>
     </section>
   );
 };
